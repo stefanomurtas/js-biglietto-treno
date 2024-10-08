3034dd04bd233c180591732e102415b57a391ad5
StefanoMurtas <stefanomurtas16@gmail.com>
2024-10-08 17:28:50 +0200

    add the correction to the fixed number and add number to convert the string to number

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
 //costante1
-const KmPercorsi= prompt ("numero di km da percorrere");
-console.log (KmPercorsi);
+const kmPercorsi= Number(prompt ("numero di km da percorrere"));
+console.log (kmPercorsi);
+
 // costante 2
-const Age= prompt ("età Viaggiatore");
-console.log (Age);
+const eta=Number( prompt ("età Viaggiatore"));
+console.log (eta);
 // costo per km 
-let prezzo= KmPercorsi* 0.21;
+let prezzo= kmPercorsi* 0.21;
 // condizione 1(se età non è maggiore di 17 allora applicare sconto 20%)
-if ( Age <= 17){
-   let UnderAge=prezzo*0.2;
-   let UnderPrice=prezzo-UnderAge;
-    console.log (UnderPrice);
-    alert( UnderPrice.toFixed(1)); 
-}else if // condizione 2(se età  è maggiore di 17 e inferiore a 65 prezzo invariato)
- (Age >=18 && Age ===64){
-    console.log (prezzo);
-    alert( prezzo.toFixed(1) );
+if ( eta <= 17){
+   let underAge=prezzo*0.2;
+   let underPrice=prezzo-underAge;
+    console.log (underPrice);
+    alert( underPrice.toFixed(2)); 
 // condizione 3(se età  è maggiore o uguale a 65 allora applicare sconto 40%)
-}else  (Age >= 65) 
+}else if (eta > 65) 
 {
-    let OverAge=prezzo*0.4;
-    let OverPrice=prezzo-OverAge;
-    console.log (OverPrice);
-    alert( OverPrice.toFixed(1) );
+    let overAge=prezzo*0.4;
+    let overPrice=prezzo-overAge;
+    console.log (overPrice);
+    alert( overPrice.toFixed(2));
 }
+else{
+    console.log (prezzo);
+    alert( overPrice.toFixed(2));
+
+}
+// else if // condizione 2(se età  è maggiore di 17 e inferiore a 65 prezzo invariato)
+//  (eta >=18|| eta == 64){
+    // console.log (prezzo);
+    // alert( prezzo.toFixed(2) );
